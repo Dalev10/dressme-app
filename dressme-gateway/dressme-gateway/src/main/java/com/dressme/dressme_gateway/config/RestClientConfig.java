e282e218a3bb768a6b2e39571d56d948ce1a9bee
Alejandro Vergara <alejandro.vergara1@udea.edu.co>
2026-06-08 16:57:12 -0500

    Refactor timeout conecction between gateway and back

--- dressme-gateway/dressme-gateway/src/main/java/com/dressme/dressme_gateway/config/RestClientConfig.java
+++ dressme-gateway/dressme-gateway/src/main/java/com/dressme/dressme_gateway/config/RestClientConfig.java
@@ -1,33 +1,42 @@
 package com.dressme.dressme_gateway.config;
 
 import com.dressme.dressme_gateway.infra.exception.BackendErrorHandler;
+import io.netty.channel.ChannelOption;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.ReactorClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
 
-    /**
-     * Builder con scope PROTOTYPE para que cada componente que lo inyecte
-     * reciba una instancia independiente.
-     */
     @Bean
     @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public RestClient.Builder restClientBuilder() {
+        
+        // 1. Configuración del cliente Netty con la sintaxis correcta
+        HttpClient httpClient = HttpClient.create()
+                // Timeout de conexión (Handshake) usando ChannelOption
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000) 
+                // Timeout de lectura (El tiempo de espera por la IA)
+                .responseTimeout(Duration.ofSeconds(60)); 
+
+        // 2. Factory correcta para inyectar Netty dentro del RestClient
+        ReactorClientHttpRequestFactory factory = new ReactorClientHttpRequestFactory(httpClient);
+
+        // 3. Construimos el builder
         return RestClient.builder()
+            .requestFactory(factory)
             .defaultStatusHandler(HttpStatusCode::isError, BackendErrorHandler::handle);
     }
 
-    /**
-     * AQUÍ ESTÁ LA MAGIA QUE FALTABA:
-     * Usamos el builder anterior para crear el RestClient final
-     * inyectando la URL del backend.
-     */
     @Bean
     public RestClient backClient(
             RestClient.Builder builder,
